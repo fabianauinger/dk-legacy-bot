35f2a56631f4a06751a4c955810a08aa11420ffd
Fabian Auinger <fabian.auinger11@gmail.com>
2025-04-28 19:12:18 +0200

    added deployment logging

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 ARRIVALS_CHANNEL_ID = 1365770366819242045
 DEPARTURES_CHANNEL_ID = 1366018627584655481
+DEPLOYMENT_LOGGING_CHANNEL_ID = 1366461721098715186
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
-    await bot.load_extension("match_signup")
+    log_channel = bot.get_channel(DEPLOYMENT_LOGGING_CHANNEL_ID)
+    if log_channel:
+        from datetime import datetime
+        timestamp = datetime.now().strftime("%d.%m.%Y %H:%M")
+        await log_channel.send(f"🚀 DK Legacy Bot wurde neu deployed ({timestamp})")
+
 
 
 @bot.event
